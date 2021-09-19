43818eaec873c1547a0be4977403f43a470f9536
Gregory Freitas <gregfreitas1997@gmail.com>
2021-09-19 15:13:59 -0400

    Server listening and working approprietly - Tries to attach to a port, and if fails then tries 5 times within   range of Port + 1000 - Regex to parse the passed message and then calculate result

---
+++ Server.java
@@ -0,0 +1,164 @@
+import java.net.Socket;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.socket.assignment.Calculator;
+import com.socket.assignment.ICalculator;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+
+public class Server 
+{
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+    private DataOutputStream output = null;
+    
+    public Server(int port)
+    {
+        
+        try
+        {
+            server = ConnectToPort(port);
+            
+            if(server == null) throw new NullPointerException("Failed to find open port.");
+            System.out.println("Server Started");
+            System.out.println("Waiting for client to connect...");
+            
+            socket = server.accept();
+            
+            System.out.println("Client accepted!");
+            
+            // Getting input from the client socket.
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            output = new DataOutputStream(socket.getOutputStream());
+
+            String line = "";
+
+            while(!line.equals("Over"))
+            {
+                try
+                {
+                    line = in.readUTF();
+
+                    
+
+                    String answ = ProcessOperation(line);
+                    
+                    output.writeUTF("Answer from server: " + answ);
+                }
+                catch(IOException e)
+                {
+                    System.err.println(e);
+                    break;
+                }
+                
+            }
+
+            System.out.println("Closing connection");
+
+            socket.close();
+            in.close();
+            server.close();
+        }
+        catch(NullPointerException e)
+        {
+            System.err.println(e);
+        }
+        catch(Exception e)
+        {
+            System.err.println(e);
+        }
+
+    }
+
+    private static ServerSocket ConnectToPort(int port)
+    {
+        ServerSocket serverSocket = null;
+        int portAttempting = port;
+        int attempts = 0;
+        Random random = new Random();
+
+        while(attempts < 5 && serverSocket == null)
+        {
+            try
+            {
+                serverSocket = new ServerSocket(portAttempting);
+                
+            }
+            catch(Exception e)
+            {
+                System.err.println(e);
+                // (Max - min + 1) + min
+                portAttempting = random.nextInt(((port + 1000) - port + 1)) + port;        
+            }
+
+            attempts++;
+        }
+
+        return serverSocket;
+    }
+
+    public static String ProcessOperation(String line)
+    {
+        Pattern p = Pattern.compile("^([\\d]+)\\s*([*/+-]{1})\\s*([\\d]+)$");
+        Matcher m = p.matcher(line);
+        
+        if(!m.matches()) return "Input error. Re-type the math question again.";
+
+        Integer x = 0, y = 0;
+        // Operators are guarenteed to be 1 Character based on the regex.
+        Character operator = m.group(2).charAt(0); 
+        x = Integer.parseInt(m.group(1));
+        y = Integer.parseInt(m.group(3));
+
+        // Little overhead since its such a small program \(o-o)/
+        ICalculator calculator = new Calculator();
+
+        float result  = 0;
+
+        switch(operator)
+        {
+            case '+':
+                result = calculator.Add(x, y);
+                break;
+            case '-':
+                result = calculator.Subtract(x, y);
+                break;
+            case '*':
+                result = calculator.Multiply(x, y);
+                break;
+            case '/':
+                try
+                {
+                    result = calculator.Divide(x, y);
+                }
+                catch(ArithmeticException e)
+                {
+                    System.err.println(e);
+                    return "Input error. Cannot divide by zero!";
+                }
+                break;
+            default:
+                return "Something went horribly wrong!";
+        }
+
+        return FormatResult(x, y, operator, result);
+    }
+
+    public static String FormatResult(float x, float y, Character op, float result)
+    {
+        return (String.valueOf(x) + op + String.valueOf(y) + '=' + String.valueOf(result));
+    }
+
+    public static void main(String[] args) {
+        //Server server = new Server(Integer.parseInt(args[0]));
+        Server server = new Server(5000);
+    }
+}
